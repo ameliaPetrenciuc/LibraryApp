a10d61d7059a76e0c7cf6f76b99896ae0524715c
Petrenciuc Amelia <amelia.petrenciuc@gmail.com>
2024-12-06 01:14:07 +0200

    Added PDF functionality

--- src/main/java/service/user/AuthentificationService.java
+++ src/main/java/service/user/AuthentificationService.java
@@ -7,5 +7,6 @@ public interface AuthentificationService {
     Notification<Boolean> register(String username, String password);
     Notification<Boolean> register(String username, String password, String role);
     Notification<User> login(String username, String password);
+    Long getLoggedInCustomerId();
     boolean logout(User user);
 }
